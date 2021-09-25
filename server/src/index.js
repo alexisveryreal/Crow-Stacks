9fa9b5db0ff6c5afebc9da7e0aa55fbaddd3fb88
alexisveryreal <alexmaple20@gmail.com>
2021-09-25 15:10:34 -0700

    setup mongoose schema + start of logs route

--- server/src/index.js
+++ server/src/index.js
@@ -2,14 +2,22 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const middlewares = require('./middlewares');
+const logs = require('./api/logs');
+
+mongoose.connect('mongodb://localhost/pancakes', {
+  useNewUrlParser: true,
+});
 
 const app = express();
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.CORS_ORIGIN,
 }));
 
 /* ROUTES */
@@ -19,10 +27,12 @@ app.get('/', (req, res) => {
   });
 });
 
+app.use('/api/logs', logs);
+
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
-const port = process.env.PORT || 1337;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
---
+++ server/src/models/PancakeEntries.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const requiredString = {
+  type: String,
+  required: true,
+};
+
+const defaultString = {
+  type: String,
+  default: '',
+};
+
+const pancakeEntrySchema = new Schema({
+  title: requiredString,
+  image: defaultString,
+  rating: {
+    type: Number,
+    min: 0,
+    max: 10,
+    default: 0,
+  },
+  address: requiredString,
+  visitDate: { type: Date, required: true },
+  description: defaultString,
+}, { timestamps: true });
+
+const Pancakes = mongoose.model('Pancakes', pancakeEntrySchema);
+
+module.exports = Pancakes;
