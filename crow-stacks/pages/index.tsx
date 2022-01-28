07441ae7e85b7ddd748a361545aca97f1e5aaddd
alexisveryreal <alexmaple20@gmail.com>
2022-01-28 15:38:22 -0800

    * Change global styles * setup index.tsx

--- crow-stacks/pages/index.tsx
+++ crow-stacks/pages/index.tsx
@@ -5,8 +5,8 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">HELLO WORLD</h1>
+    <div className="items-center justify-center flex flex-col animate-fade-in-down">
+      <h1 className="text-3xl font-bold underline">CROW</h1>
     </div>
   );
 };
