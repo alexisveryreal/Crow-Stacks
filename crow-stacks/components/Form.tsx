85686ecd3ff0e2e6318f9acacd957d1dc2c6cd6b
alexisveryreal <alexmaple20@gmail.com>
2022-01-28 15:48:41 -0800

    * added next-UI * added simple text area will expand on it later

---
+++ crow-stacks/components/Form.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Textarea } from "@nextui-org/react";
+
+const Form = () => {
+  return (
+    <div>
+      <Textarea
+        label="Why are we still here?"
+        placeholder="Enter your dumb ideas."
+      />
+    </div>
+  );
+};
+
+export default Form;
--- crow-stacks/pages/index.tsx
+++ crow-stacks/pages/index.tsx
@@ -2,11 +2,13 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Form from "../components/Form";
 
 const Home: NextPage = () => {
   return (
     <div className="items-center justify-center flex flex-col animate-fade-in-down">
       <h1 className="text-3xl font-bold underline">CROW</h1>
+      <Form />
     </div>
   );
 };
