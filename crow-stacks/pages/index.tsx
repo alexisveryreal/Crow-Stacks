1175057b515700cc29260778c01eb354a9a0e733
alexisveryreal <alexmaple20@gmail.com>
2022-01-28 16:25:28 -0800

    * center everything, need ideas...

--- crow-stacks/pages/index.tsx
+++ crow-stacks/pages/index.tsx
@@ -1,16 +1,14 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import Form from "../components/Form";
 
 // Test commit
 
 const Home: NextPage = () => {
   return (
-    <div className="items-center justify-center flex flex-col animate-fade-in-down">
+    <div className="items-center justify-center h-screen w-screen flex flex-col animate-fade-in-down">
       <h1 className="text-3xl font-bold underline">CROW</h1>
       <Form />
+      <div></div>
     </div>
   );
 };
